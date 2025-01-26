27d4ec537fbee48a258143685427766f6aaaa30b
Berrzzerk <imadber@outlook.com>
2025-01-26 18:01:14 +0100

    Loading

--- loading.js
+++ loading.js
@@ -1,7 +1,7 @@
 // Simulate loading and redirect to another page
 window.addEventListener("load", () => {
     setTimeout(() => {
-        window.location.href = "index.html"; // Replace with the URL of your actual site
+        window.location.href = "main.html"; // Replace with the URL of your actual site
     }, 4000); // 2000 ms = 2 seconds
     
 });
